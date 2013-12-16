5b534cc34871fc2ad81f2881500ee22eee993c3f
tisane <nathanlarson3141@gmail.com>
2013-12-16 03:28:06 -0500

    New extension

---
+++ EmailAllChanges.i18n.php
@@ -0,0 +1,7 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+    'tog-emailallchanges' => 'Email me when any page on the wiki is changed',
+    'emailallchanges-desc' => 'Adds a preferences checkbox allowing users to have all changes to pages on the wiki emailed to them.'
+);
