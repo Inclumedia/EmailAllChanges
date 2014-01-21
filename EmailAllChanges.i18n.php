05fb14d4667ce75ff4de97235c25782e233f7a6b
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-01-21 11:31:21 +0100

    Update EmailAllChanges.i18n.php
    * Added file description
    * Small tweaks to the English original
    * Added German translation

--- EmailAllChanges.i18n.php
+++ EmailAllChanges.i18n.php
@@ -1,7 +1,25 @@
 <?php
+/**
+* Internationalization file for the EmailAllChanges extension.
+*
+* @licence GPL v2 or later
+* @author Nathan Larson (Leucosticte)
+*/
+
 $messages = array();
 
+/** English
+* @author Leucosticte
+*/
 $messages['en'] = array(
-    'tog-emailallchanges' => 'Email me when any page on the wiki is changed',
-    'emailallchanges-desc' => 'Adds a preferences checkbox allowing users to have all changes to pages on the wiki emailed to them.'
+    'emailallchanges-desc' => 'Adds a preferences checkbox allowing users to have all changes to pages on the wiki emailed to them',
+    'tog-emailallchanges' => 'Email me when any page on the wiki is changed'
+);
+
+/** German (Deutsch)
+* @author Kghbln
+*/
+$messages['de'] = array(
+    'tog-emailallchanges' => 'Schicke mir E-Mails zu jeder Seitenänderung im Wiki',
+    'emailallchanges-desc' => 'Ergänzt die Benutzeroption sich E-Mail-Benachrichtigungen zu allen Seitenänderungen des Wikis senden zu lassen'
 );
